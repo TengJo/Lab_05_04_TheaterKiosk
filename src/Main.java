98c598206e091353499bc6043eaadbd96cc02f8c
TengJo <tengjh@mail.uc.edu>
2022-02-13 21:19:13 -0500

    Changed age to a false value

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main
     public static void main(String[] args)
     {
         int age;
-        age = 21;
+        age = 20;
             if (age >= 21)
             {
                 System.out.println("You get a paper wrist band.");
